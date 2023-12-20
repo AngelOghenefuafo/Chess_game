8ccfdab8851e2fc6005fbf54cfbfcd03fe76fa3e
AngelOgehnefuafo <angeloghenefuafo@gmail.com>
2023-12-20 20:15:09 +0100

    Correct moves

--- app.js
+++ app.js
@@ -68,19 +68,21 @@ function dragOver(e) {
 
 function dragDrop(e) {
     e.stopPropagation()
-    console.log('playerGo', playerGo)
-
     console.log('e.target', e.target)
     const correctGo = draggedElement.firstChild.classList.contains(playerGo)
     const taken = e.target.classList.contains('piece')
     const opponentGo = playerGo === 'white' ? 'black' : 'white'
-    console.log('opponentGo', opponentGo)
     const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
 
-    //e.target.parentNode.append(draggedElement)
-    //e.target.remove()
+    if (correctGo) {
+        //check if valid
+        if (taken && !takenByOpponent) {
+            infoDisplay.textContent = "you can't move here!"
+            setTimeout(() => infoDisplay.textContent = "", 2000)
+            return
+        }
+    }
     //e.target.append(draggedElement)
-    changePlayer()
 }
 
 function changePlayer() {
